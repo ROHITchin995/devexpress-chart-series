430f63de120ee76b31aa5e6f8005701330773bcf
ROHITchin995 <rohitsure.9915@gmail.com>
2023-02-06 22:14:09 +0530

    fetch logic added

--- src/QuestData.js
+++ src/QuestData.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+
+const HOST = "https://demo.questdb.io";
+
+
 
 export const QuestData = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        run();
+      }, []);
+
+    async function run() {
+        try {
+          const query = `SELECT pickup_datetime,trip_distance from trips LIMIT ${fetchLimit.lowerlimit},${fetchLimit.upperlimit};`;
+    
+          const response = await fetch(
+            `${HOST}/exec?query=${encodeURIComponent(query)}`
+          );
+          const json = await response.json();
+    
+          setData(json);
+          // console.log(json);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
   return (
     <div>QuestData</div>
   )
